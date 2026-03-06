a761f245e9427002147c8cfc2f0ce48437f18cfe
Pandhari-404 <pandharipawde018@gmail.com>
2026-03-06 16:22:35 +0530

    Fix small error

--- coffee/urls.py
+++ coffee/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path('add-to-cart/<int:coffee_id>/', views.add_to_cart, name='add_to_cart'),
     path('update-cart/<int:item_id>/', views.update_cart_quantity, name='update_cart_quantity'),
     path('remove-cart-item/<int:item_id>/', views.remove_cart_item, name='remove_cart_item'),
-    path('checkout/', views.checkout, name='checkout'),
     path('about/', views.About, name='about'),
     path('mycart/', views.My_Cart, name='mycart'),
+    path('checkout/', views.checkout, name='checkout'),
     path('contact/', views.Contact, name='contact'),
     path("order-success/", views.order_success, name="order_success"),
     
