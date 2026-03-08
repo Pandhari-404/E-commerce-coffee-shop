cdf6ef9c903d67875e2f7b139d6831eafa2ef253
Pandhari-404 <pandharipawde018@gmail.com>
2026-03-08 10:38:15 +0530

    Fix contact form server error

--- coffee/views.py
+++ coffee/views.py
@@ -7,7 +7,7 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.core.mail import send_mail
 from django.conf import settings
-from django.shortcuts import render
+# from django.shortcuts import render
 # Create your views here.
 
 
